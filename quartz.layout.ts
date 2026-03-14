a9d2e3e8a9553c17b50d307fca28608c68635616
delcarsdungeon <61684859+delcarsdungeon@users.noreply.github.com>
2026-03-14 03:48:58 -0400

    Update quartz.layout.ts
    Removed redundant table of contents on the main index page.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -46,9 +46,12 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-  ],
+  Component.Graph(),
+  Component.ConditionalRender({
+    component: Component.DesktopOnly(Component.TableOfContents()),
+    condition: (page) => page.fileData.slug !== "index",
+  }),
+],
 }
 
 // components for pages that display lists of pages (e.g. tags or folders)
