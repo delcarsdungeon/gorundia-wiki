3d8e618437d9377cbf4689921eb15212d3ba88fe
delcarsdungeon <61684859+delcarsdungeon@users.noreply.github.com>
2026-03-14 03:31:01 -0400

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -56,7 +56,6 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
   ],
 }
 
